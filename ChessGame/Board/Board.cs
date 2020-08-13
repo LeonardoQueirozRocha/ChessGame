84bef438f233faa9bd1617ea290c106fc87c7f1a
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2020-08-13 20:33:44 -0300

    Treating erros with an custom exception

--- ChessGame/Board/Board.cs
+++ ChessGame/Board/Board.cs
@@ -20,11 +20,40 @@ namespace ChessGame.board
         {
             return pieces[line, column];
         }
+        public Piece piece(Position position)
+        {
+            return pieces[position.Line, position.Column];
+        }
 
+        public bool PieceExist(Position position)
+        {
+            ValidatePosition(position);
+            return piece(position) != null;
+        }
         public void PutPiece(Piece piece, Position position)
         {
+            if (PieceExist(position))
+            {
+                throw new BoardException("Already exists an piece in this position!");
+            }
             pieces[position.Line, position.Column] = piece;
             piece.Position = position;
+        }
+
+        public bool ValidPosition(Position position)
+        {
+            if (position.Line < 0 || position.Line >= Lines || position.Column < 0 || position.Column >= Columns)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void ValidatePosition(Position position)
+        {
+            if (!ValidPosition(position))
+            {
+                throw new BoardException("Invalid Position!");
+            }
         }
     }
 }
--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -10,15 +10,20 @@ namespace ChessGame
     {
         static void Main(string[] args)
         {
-            Board board = new Board(8, 8);
+            try
+            {
+                Board board = new Board(8, 8);
 
-            board.PutPiece(new Tower(board, Color.Black), new Position(0, 0));
-            board.PutPiece(new Tower(board, Color.Black), new Position(1, 3));
-            board.PutPiece(new King(board, Color.Black), new Position(2, 4));
+                board.PutPiece(new Tower(board, Color.Black), new Position(0, 0));
+                board.PutPiece(new Tower(board, Color.Black), new Position(1, 9));
+                board.PutPiece(new King(board, Color.Black), new Position(0, 2));
 
-            Screen.PrintTray(board);
-
-
+                Screen.PrintTray(board);
+            }
+            catch (BoardException error)
+            {
+                Console.WriteLine(error.Message);
+            }
             Console.ReadLine();
         }
     }
